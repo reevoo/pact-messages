fcc3c716701ed58d8cab7585397baa04eedc09ab
David Sevcik <david.sevcik@gmail.com>
2015-11-05 12:38:44 +0100

    Initial version of mock service definition spec

---
+++ spec/integration/mock_service_definition_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'Mock service definition' do
+  let(:world) { Pact::Messages::Consumer::World.new }
+  let(:contract_builder) do
+    Pact::Messages::Consumer::ContractBuilder.new(
+      consumer_name: 'My Consumer',
+      provider_name: 'My Provider',
+      pact_dir: '/tmp'
+    )
+  end
+
+  before do
+    allow(Pact::Messages).to receive(:consumer_world).and_return(world)
+    world.register_mock_service(:my_service, contract_builder)
+
+    Pact::Messages.build_mock_service(:my_service) do |service|
+      service
+        .given('provider state 1')
+        .provide(foo: like('bar1'))
+        .given('provider state 2')
+        .provide(foo: like('bar2'))
+
+      service.provide(foo: like('default bar'))
+    end
+  end
+
+  it 'registers the contract interactions' do
+    expect(Pact::Messages.get_interaction_response('My Provider', 'My Consumer')).to eq(foo: 'default bar')
+    expect(Pact::Messages.get_interaction_response('My Provider', 'My Consumer', 'provider state 1')).to eq(foo: 'bar1')
+    expect(Pact::Messages.get_interaction_response('My Provider', 'My Consumer', 'provider state 2')).to eq(foo: 'bar2')
+  end
+end

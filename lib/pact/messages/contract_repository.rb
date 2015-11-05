3685c3c8565a63b5e046f2008833e285ed3199e6
David Sevcik <david.sevcik@gmail.com>
2015-11-05 10:45:39 +0100

    Rename get_contract into get_intercation_response

--- lib/pact/messages/contract_repository.rb
+++ lib/pact/messages/contract_repository.rb
@@ -7,10 +7,14 @@ module Pact::Messages
   module ContractRepository
     module_function
 
-    def get_contract(provider_name, consumer_name, provider_state = nil, options = {})
-      # search for it localy, if not found look on the pack broker
-      contract = Pact::Messages.consumer_world.find_contract(provider_name, consumer_name) ||
-        Pact::ConsumerContract.from_uri(pact_path(provider_name, consumer_name))
+    def get_contract(provider_name, consumer_name)
+      # search for it locally, if not found look on the pack broker
+      Pact::Messages.consumer_world.find_contract(provider_name, consumer_name) ||
+        Pact::ConsumerContract.from_uri(pact_broker_url(provider_name, consumer_name))
+    end
+
+    def get_interaction_response(provider_name, consumer_name, provider_state = nil, options = {})
+      contract = get_contract(provider_name, consumer_name)
       fail "Contract between #{provider_name} and #{consumer_name} not found" unless contract
       find_response_for_provider_state(contract.interactions, provider_state, options)
     end
@@ -19,14 +23,14 @@ module Pact::Messages
       { reificate: true }
     end
 
-    def pact_path(provider, consumer)
+    def pact_broker_url(provider, consumer)
       url = URI.encode("/pacts/provider/#{provider}/consumer/#{consumer}/")
       URI(ENV.fetch('PACT_BROKER_URL') + url + 'latest')
     end
 
     def find_response_for_provider_state(interactions, state, options = {})
       options.reverse_merge!(default_contract_options)
-      interaction = interactions.select { |i| i.provider_state == state }.first
+      interaction = interactions.find { |i| i.provider_state == state }
       body = interaction.response[:body]
       options[:reificate] ? ::Pact::Reification.from_term(body) : body
     end
